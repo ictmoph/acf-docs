39921ccdabb2d4c714ff95c5342260cce56bd597
Leenawat Papahom <leenawat.pa@gmail.com>
2021-04-26 14:27:55 +0700

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    title: 'acf-docs V20210426.10.13',
+    description: '',
+    base: '/acf-docs/',
+    port: '4000',
+    sorter: true,
+    themeConfig: {
+        nav: [
+            { text: 'คู่มือผู้ใช้งาน', link: '/user/login' },
+            { text: 'ดาวน์โหลด', link: '/download/' },
+            // { text: 'foo', link: '/foo/one' },
+            // { text: 'bar', link: '/bar/one' },
+        ],
+        sidebar: [
+            {
+                title: 'คู่มือผู้ใช้งาน',
+                collapsable: true,
+                children: [
+                    '/user/login',
+                    '/user/user-menu',
+                    '/user/admin-hosp-menu',
+                    '/user/admin-ssj-menu',
+                    '/user/prepare-work',
+                    '/user/working'
+                ]
+            },
+            '/Q&A/',
+            '/contact/',
+            '/download/',
+            {
+                title: 'การติดตั้งเครื่องอ่านบัตรประจำตัวประชาชนแบบ Smartcard',
+                collapsable: true,
+                children: [
+                    '/smartcard/install',
+                    '/smartcard/troubleshooting'
+                ]
+            },
+            '/request_permission/',
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+## คู่มือการใช้งาน Co-Vaccine
+
+1. [ภาพรวมขั้นตอนการให้บริการ](https://ict.moph.go.th/upload_file/files/c2ee124ca87ed0f4a23bab985ba16246.pdf)
+1. [คู่มือการใช้งาน](../user/login.md)
+1. [ถาม-ตอบ ระบบ Co-Vaccine](https://docs.google.com/document/u/0/d/1_6qTG-n3PZt_2mOQiUma_ov1snj_FXaucPHIR5lma4A/mobilebasic)
+1. [ช่องทางติดต่อสื่อสาร](../user/login.md)
+1. [ดาวน์โหลดเอกสาร](../contact/index.md)
+1. [การติดตั้งเครื่องอ่านบัตรประจำตัวประชาชนแบบ Smartcard](../smartcard/install.md)
+
+>เมนูเพิ่มเติมอื่น ๆ สามารถกดปุ่มไอคอนแฮมเบอร์เกอร์
+
+![Logo](./img/menu-top.png)
+
+### Version 0.1.7 Update 15/04/2021 11:39 AM
